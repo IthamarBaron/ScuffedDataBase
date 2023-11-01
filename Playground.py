ac958e391810e83906266c9ad0eeeef52930be61
Ithamar <57110382+IthamarBaron@users.noreply.github.com>
2023-11-01 17:07:16 +0200

    fixed miss-understanding about the 2 classes in part a/b now the first class handles the dict. the second one handles the database

--- Playground.py
+++ Playground.py
@@ -1,7 +1,6 @@
 # this file is used as a testing area
-from DataBase import Database
+from LocalDict import HandleDict
 
 import pickle
-with open("database.pkl",'rb') as f:
-    print(pickle.load(f))
+
 
