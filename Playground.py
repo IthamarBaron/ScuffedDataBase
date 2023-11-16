c647a6e40c5a93c015a99a8eb963af88a9a5ee5c
Ithamar <Ithamar.baron@mgil.com>
2023-11-16 11:56:10 +0200

    shared memory basic structure in the playground

--- Playground.py
+++ Playground.py
@@ -1,33 +1,28 @@
 import multiprocessing
+from multiprocessing import shared_memory
+import  time
 
-# Create shared memory for data and a counter
-data_memory = multiprocessing.Array('c', b'\x00' * 100)
-counter_memory = multiprocessing.Value('i', 0)
-print("a")
-def test(key):
-    # Read the current counter value and update it
-    with counter_memory.get_lock():
-        counter = counter_memory.value
-        counter_memory.value += 1
 
-    # Calculate the start position for the new data
-    start_position = counter * len(key)
-    # Write the key to the shared memory
-    data_memory[start_position:start_position + len(key)] = key.encode('utf-8')
-    data = bytearray(data_memory[:]).decode('utf-8').rstrip('\x00')
-    print("1Data in shared memory:", repr(data))  # Print the repr to see non-printable characters
-if __name__ == "__main__":
-    # Start two processes with the test function and different arguments
-    p1 = multiprocessing.Process(target=test, args=("one",))
-    p2 = multiprocessing.Process(target=test, args=("two",))
+def save_memory(value):
+    shm = shared_memory.SharedMemory(name='test', create=False, size=4)
+    print(f"in shared memory a: {type(value)}")
+    i = shm.buf[0]
+    # Convert bytes to memoryview before assignment
+    value_memoryview = memoryview(value)
+    shm.buf[i:(i + len(value_memoryview))] = value_memoryview
+    print(f"Starting at [{i} : {i+len(value_memoryview)}] list is: {shm.buf.tolist()}")
+    shm.buf[0] = i + len(value_memoryview)
 
-    p1.start()
+if __name__ == '__main__':
+    print("created shared memory")
+    shm = shared_memory.SharedMemory(name='test', create=True, size=4)
+    shm.buf[0] = 1
+    p = multiprocessing.Process(target=save_memory, args=(b"a",))
+    p2 = multiprocessing.Process(target=save_memory, args=(b"b",))
+
+    p.start()
     p2.start()
-
-    p1.join()
+    p.join()
     p2.join()
+    print(shm.buf.tolist())
 
-    # Read the data from the shared memory
-    data = bytearray(data_memory[:]).decode('utf-8').rstrip('\x00')
-
-    print("2Data in shared memory:", repr(data))  # Print the repr to see non-printable characters
